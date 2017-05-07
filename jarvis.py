c0f18dd140a70372bb4861fd0415b777cf4921ac
Shefali Munjal <shefali.jmit@gmail.com>
2017-05-07 16:18:57 -0700

    Revert "Replace jarvis.py"
    This reverts commit fd0f90ec2e7a8b6db01c3c93eaa04d1516fec5a3.

--- jarvis.py
+++ jarvis.py
@@ -1,60 +1,40 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+import json
+
 import os
 import time
 from slackclient import SlackClient
-import sys
-sys.path.append('./scripts/classifier')
-sys.path.append('./scripts/server/sql')
-from classifier import Classifier
-import utils, instructor, course, program_outcomes, learning_objectives, lab_schedule, \
-    assignment_schedule, project_schedule, mid_term_schedule, final_exam_schedule, \
-    course_grading
+
+channel = None
+flag = 0
+voice_text = None
 
 
-# jarvis's ID as an environment variable
-BOT_ID = 'U55PR6EK1'
-SLACK_BOT_TOKEN = 'xoxb-175807218647-HL9jcD2OAzBxgj5G0QBrwcfX'
+# starterbot's ID as an environment variable
+BOT_ID = os.environ.get("BOT_ID")
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
+EXAMPLE_COMMAND = "do"
 
 # instantiate Slack & Twilio clients
-slack_client = SlackClient(SLACK_BOT_TOKEN)
-
-#instantiate classifier
-txt_clf = Classifier()
-
-def _get_answer(text):
-    text = text.lower()
-    text = utils.map_words_to_digits_in_text(text)
-    label = txt_clf.classify(text)
-
-    if label == 'instructor':
-        return instructor.get_instructor_details(text)
-    elif label == 'course_name':
-        return course.get_course_details(text)
-    elif label == 'course_learning_objectives':
-        return learning_objectives.get_learning_objectives(text)
-    elif label == 'program_outcome':
-        return program_outcomes.get_program_outcomes(text)
-    elif label == 'lab_schedule':
-        return lab_schedule.get_lab_schedule(text)
-    elif label == 'assignment_schedule':
-        return assignment_schedule.get_assignment_schedule(text)
-    elif label == 'project_schedule':
-        return project_schedule.get_project_schedule(text)
-    elif label == 'mid_term_schedule':
-        return mid_term_schedule.get_mid_term_schedule(text)
-    elif label == 'final_exam_schedule':
-        return final_exam_schedule.get_final_exam_schedule(text)
-    elif label == 'course_grading':
-        return course_grading.get_course_grading_details(text)
-    else:
-        return "Sorry I don't understand your question"
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 
 def handle_command(command, channel):
-    response = _get_answer(command)
-    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
+    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
+               "* command with numbers, delimited by spaces."
+    if command.startswith(EXAMPLE_COMMAND):
+        response = "Sure...write some more code then I can do that!"
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
 
 
 def parse_slack_output(slack_rtm_output):
@@ -67,6 +47,7 @@ def parse_slack_output(slack_rtm_output):
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
+
                 # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), \
                        output['channel']
@@ -75,11 +56,39 @@ def parse_slack_output(slack_rtm_output):
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    channel = None
     if slack_client.rtm_connect():
-        print("Jarvis connected and running!")
+        print("StarterBot connected and running!")
         while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-            if command and channel:
+            if channel is not None and flag == 0:
+                r = sr.Recognizer()
+                with sr.Microphone() as source:
+                    print("Say something!")
+                    response1 = "Say something!"
+                    slack_client.api_call("chat.postMessage", channel=channel,
+                                          text=response1, as_user=True)
+                    audio = r.listen(source)
+
+                    if r.recognize_google(audio):
+                        flag = 1
+                        voice_text = r.recognize_google(audio)
+                        response2 = "you said " + voice_text
+                        slack_client.api_call("chat.postMessage", channel=channel,text=response2, as_user=True)
+
+            if flag==0 and channel is None:
+                val_comm, val_cha = parse_slack_output(slack_client.rtm_read())
+                command = val_comm
+                channel = val_cha
+
+            if flag==1:
+                command = voice_text
+                flag = 0
+
+            if command:
+                print "command "
+                print command
+                print "channel"
+                print channel
                 handle_command(command, channel)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
