54a3a3844ab41c06c21b0e6e8575129219c73e29
Shefali Munjal <shefali.munjal@sjsu.edu>
2018-05-14 11:57:44 -0700

    minor changes in server response

--- scripts/server/sql/reference_materials.py
+++ scripts/server/sql/reference_materials.py
@@ -3,6 +3,7 @@ from course_section import *
 
 def get_reference_material(text):
     course = get_course(text)
+    response = {}
     if not course:
         return {"error":"Sorry no matching courses found. valid courses are: " + str(get_all_course_names())}
 
@@ -16,5 +17,6 @@ def get_reference_material(text):
 
     if not course_section_details:
         return {"error":"Sorry no reference material found for " + course_section_name}
-
-    return course_section_name + " references are " + course_section_details['reference_material']
+    response["references"] = course_section_details['reference_material']
+    response["course_name"] = course['name']
+    return response
